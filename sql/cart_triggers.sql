5ee1a3a58d618d7290af65a5d775e4a249952181
passionee <9622541+passionee@users.noreply.github.com>
2023-06-08 21:03:05 -0700

    Added triggers for cart calculations

---
+++ sql/cart_triggers.sql
@@ -0,0 +1,35 @@
+delimiter |
+CREATE TRIGGER cart_add_item BEFORE INSERT ON client_cart_item
+    FOR EACH ROW
+    BEGIN
+        UPDATE client_cart SET client_cart.cart_subtotal = client_cart.cart_subtotal + (NEW.quantity * NEW.price)
+        WHERE client_cart.id=NEW.cart_id;
+        UPDATE client_cart SET client_cart.cart_total = IF(client_cart.cart_subtotal > 0, client_cart.cart_subtotal + client_cart.cart_tax, 0)
+        WHERE client_cart.id=NEW.cart_id;
+    END;
+|
+
+CREATE TRIGGER cart_update_item BEFORE UPDATE ON client_cart_item
+    FOR EACH ROW
+    BEGIN
+        UPDATE client_cart SET client_cart.cart_subtotal = client_cart.cart_subtotal - (OLD.quantity * OLD.price)
+        WHERE client_cart.id=NEW.cart_id;
+        UPDATE client_cart SET client_cart.cart_subtotal = client_cart.cart_subtotal + (NEW.quantity * NEW.price)
+        WHERE client_cart.id=NEW.cart_id;
+        UPDATE client_cart SET client_cart.cart_total = IF(client_cart.cart_subtotal > 0, client_cart.cart_subtotal + client_cart.cart_tax, 0)
+        WHERE client_cart.id=NEW.cart_id;
+    END;
+|
+
+CREATE TRIGGER cart_delete_item BEFORE DELETE ON client_cart_item
+    FOR EACH ROW
+    BEGIN
+        UPDATE client_cart SET client_cart.cart_subtotal = client_cart.cart_subtotal - (OLD.quantity * OLD.price)
+        WHERE client_cart.id=OLD.cart_id;
+        UPDATE client_cart SET client_cart.cart_total = IF(client_cart.cart_subtotal > 0, client_cart.cart_subtotal + client_cart.cart_tax, 0)
+        WHERE client_cart.id=OLD.cart_id;
+    END;
+
+|
+delimiter ;
+
